b53e938366f1a8dfd10545b18ec15243d91814ad
Bumble <bumble.sakh@gmail.com>
2022-02-02 02:19:25 +1100

    feat: add playlist obj

---
+++ audio-player/index.js
@@ -0,0 +1,20 @@
+const playlist = [
+  {
+    artist: 'Coolio feat. L.V.',
+    track: "Gangsta's Paradise",
+    cover: 'coolio-gangstas-paradise',
+    file: 'coolio-gangstas-paradise',
+  },
+  {
+    artist: 'ATB',
+    track: '9 PM',
+    cover: 'atb-9-pm',
+    file: 'atb-9-pm',
+  },
+  {
+    artist: 'Dr. Alban',
+    track: "It's My Life",
+    cover: 'dr-alban-its-my-life',
+    file: 'dr-alban-its-my-life',
+  },
+]
