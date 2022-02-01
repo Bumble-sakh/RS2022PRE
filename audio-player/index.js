fb7ccc04d4890b62f4b986c62c2222efb472a196
Bumble <bumble.sakh@gmail.com>
2022-02-02 03:06:05 +1100

    feat: add Player class

--- audio-player/index.js
+++ audio-player/index.js
@@ -18,3 +18,38 @@ const playlist = [
     file: 'dr-alban-its-my-life',
   },
 ]
+
+class Player {
+  constructor(playlist) {
+    this.playlist = playlist
+    this.currentTrack = 0
+    this.track = new Audio(
+      `assets/playlist/${this.playlist[this.currentTrack].file}.mp3`
+    )
+  }
+  get isPlay() {
+    return !this.track.paused
+  }
+  play() {
+    this.track.play()
+  }
+  pause() {
+    this.track.pause()
+  }
+}
+
+const player = new Player(playlist)
+
+const playBtn = document.querySelector('.play-btn')
+playBtn.addEventListener('click', (e) => {
+  if (player.isPlay) {
+    player.pause()
+    playBtn.classList.remove('uil-pause-circle')
+    playBtn.classList.add('uil-play-circle')
+  } else {
+    player.play()
+
+    playBtn.classList.remove('uil-play-circle')
+    playBtn.classList.add('uil-pause-circle')
+  }
+})
